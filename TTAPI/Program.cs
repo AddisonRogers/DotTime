b9a183956458965f8455b2d6f68b054f8add8b2c
AddisonRogers <AddisonRogers@protonmail.com>
2023-10-20 14:31:12 +0100

    Add JWT authentication and CORS support to application

--- TTAPI/Program.cs
+++ TTAPI/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+//https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis/security?view=aspnetcore-7.0
