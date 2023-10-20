53376a059b2d027fd6e9020ece7e94de22a0b4f9
AddisonRogers <AddisonRogers@protonmail.com>
2023-10-20 16:10:27 +0100

    Base

---
+++ DTWorker/Worker.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+
+namespace DotTime;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+            Process[] localAll = Process.GetProcesses();
+            //var processes = new Dictionary<string, _process>();
+            foreach (var process in localAll)
+            {
+                process.EnableRaisingEvents = true;
+                process.Exited += (o, eventArgs) => ProcessExited(o, eventArgs, process);
+                
+                //*
+                //if (!processes.TryAdd(process.ProcessName, new _process(default, true, process.StartTime)))
+                //{ // Activates when there is something already in there with the process name
+                //    if (!processes.TryGetValue(process.ProcessName, out var val)) throw new Exception("idk message Addison");
+                //    
+                //    processes[process.ProcessName] = new _process(val.Uptime, isRunning: true, process.StartTime); 
+                //    // So if we have the process in database we update it to make it so it is running
+                //}; 
+                //*
+            }
+
+            await Task.Delay(1000, stoppingToken);
+            continue;
+
+            void ProcessExited(object? sender, EventArgs eventArgs, Process process)
+            {
+                
+                //if (!processes.TryGetValue(process.ProcessName, out var val)) throw new Exception("Failed to get process from dictionary");
+                //processes[process.ProcessName] = new _process(val.Uptime + (Now - process.StartTime), isRunning: false, MinValue); 
+                //*
+                // When the process is exited then we flip the running value and update the uptime 
+            }
+        }
+    }
+}
+public abstract class ProcessItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsRunning { get; set; }
+    public List<ProcessRecord> History { get; set; }
+}
+public readonly record struct ProcessRecord(int StartTime, int EndTime)
+{ 
+    public int Uptime => EndTime - StartTime;
+};
