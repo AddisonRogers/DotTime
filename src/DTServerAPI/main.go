bcd8c291e07a79061c726cc11b07cbad3460aa86
AddisonRogers <AddisonRogers@protonmail.com>
2023-11-12 16:17:02 +0000

    Add process tracking functionality with Mongo

--- src/DTServerAPI/main.go
+++ src/DTServerAPI/main.go
@@ -1,62 +1,91 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
+	"time"
 )
 
-type updateProcess struct {
-	id           int
-	name         string
-	timeStarted  int
-	timeEnded    int
-	timeDuration int
-	threads      int
-	memoryUsage  int
+type Process struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	TimeStarted string `json:"timeStarted"`
+	IsRunning   bool   `json:"isRunning"`
+	TimeEnded   string `json:"timeEnded"`
+	Duration    string `json:"duration"`
+	Threads     string `json:"threads"`
+	MemoryUsage string `json:"memoryusage"`
 }
 
-type process struct {
-	id          int
-	name        string
-	history     []processTime
-	threads     []int
-	memoryUsage []int
-}
-
-type processTime struct {
-	timeStarted  int
-	timeEnded    int
-	timeDuration int
-}
-
-func conv(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Default().Println(err)
-	}
-	return i
+type Doc struct {
+	Token     string    `json:"token"`
+	Processes []Process `json:"proccesses"`
 }
 
 func main() {
+	// main context for client creation
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// client creation using the context created above
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	collection := client.Database("db").Collection("processes")
+
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
 	e.POST("/process", func(c echo.Context) error {
-		token := c.Request().Header.Get("token") // Log the process to the token primary key
-		_process := updateProcess{
-			id:           conv(c.FormValue("id")),
-			name:         c.FormValue("name"),
-			timeStarted:  conv(c.FormValue("timeStarted")),
-			timeEnded:    conv(c.FormValue("timeEnded")),
-			timeDuration: conv(c.FormValue("timeDuration")),
-			threads:      conv(c.FormValue("threads")),
-			memoryUsage:  conv(c.FormValue("memoryUsage")),
+		// separate context with its own timeout for each request
+		reqCtx, reqCancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer reqCancel()
+
+		token := c.Request().Header.Get("token")
+		process := Process{
+			Id:          c.FormValue("id"),
+			Name:        c.FormValue("name"),
+			TimeStarted: c.FormValue("timeStarted"),
+			IsRunning:   c.FormValue("isRunning") == "true",
+			TimeEnded:   c.FormValue("timeEnded"),
+			Duration:    c.FormValue("timeDuration"),
+			Threads:     c.FormValue("threads"),
+			MemoryUsage: c.FormValue("memoryUsage"),
 		}
 
+		filter := bson.D{{Key: "token", Value: token}}
+		update := bson.D{{Key: "$push", Value: bson.D{{Key: "proccesses", Value: process}}}}
+
+		upsert := true
+		after := options.After
+		opts := options.FindOneAndUpdateOptions{
+			ReturnDocument: &after,
+			Upsert:         &upsert,
+		}
+
+		err = collection.FindOneAndUpdate(reqCtx, filter, update, &opts).Err()
+		if err != nil {
+			log.Fatal(err)
+			return c.String(http.StatusInternalServerError, "There was a problem with the request.")
+		}
+		fmt.Println("Request success.")
 		return c.String(http.StatusOK, "Request success.")
 	})
 
