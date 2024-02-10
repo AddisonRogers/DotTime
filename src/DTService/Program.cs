99db99b7bfa844a3023b0b23aef20ca271918c38
AddisonRogers <AddisonRogers@protonmail.com>
2024-02-10 19:11:28 +0000

    Deleting everything

--- src/DTService/Program.cs
+++
@@ -1,8 +0,0 @@
-using DTService;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-builder.Services.AddSingleton<Worker>();
-
-var host = builder.Build();
-host.Run();
